a54f3ea7b9238a943b253b6a39257a0ff0d4e864
Sathyan S S <sssathyan2004@gmail.com>
2025-12-24 12:27:05 +0530

    Update traffic_tb.v

--- 05. FSM/03. Controller Circuits/01. Traffic Light Controller/traffic_tb.v
+++ 05. FSM/03. Controller Circuits/01. Traffic Light Controller/traffic_tb.v
@@ -19,10 +19,10 @@ always @(posedge clk) begin
 		$display("");
 		$display("Traffic Light Controller");
 		$display("------------------------");
-		$display("From East 	: Left=%b | Right=%b | Opposite=%b",e2_l, e2_r, e2_o);
-		$display("From West 	: Left=%b | Right=%b | Opposite=%b",w2_l, w2_r, w2_o);
-		$display("From North 	: Left=%b | Right=%b | Opposite=%b",n2_l, n2_r, n2_o);
-		$display("From South 	: Left=%b | Right=%b | Opposite=%b",s2_l, s2_r, s2_o);
+		$display("From East		: Left=%b | Right=%b | Opposite=%b",e2_l, e2_r, e2_o);
+		$display("From West		: Left=%b | Right=%b | Opposite=%b",w2_l, w2_r, w2_o);
+		$display("From North	: Left=%b | Right=%b | Opposite=%b",n2_l, n2_r, n2_o);
+		$display("From South	: Left=%b | Right=%b | Opposite=%b",s2_l, s2_r, s2_o);
 		$display("Time taken 	: %0t",$time);
 		$display("");
 	end
